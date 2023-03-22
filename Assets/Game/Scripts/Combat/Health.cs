eaf94cd12d6fa6d8b9b785cc8ceaace74aa285b9
marcell-lovassy <marcell.lovassy@gmail.com>
2023-03-22 13:10:14 +0100

    Add death animation to the character animator

--- Assets/Game/Scripts/Combat/Health.cs
+++ Assets/Game/Scripts/Combat/Health.cs
@@ -6,22 +6,41 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
+        const string DEATH_TRIGGER = "DeathTrigger";
+
+        public bool IsDead => !isAlive;
+
         [SerializeField]
         private float maxHealth;
 
-        float health;
+        float healthPoints;
+        bool isAlive;
+        private Animator animator;
 
 
         // Start is called before the first frame update
         void Start()
         {
-            health = maxHealth;
+            healthPoints = maxHealth;
+            animator = GetComponent<Animator>();
+            isAlive = true;
         }
 
         public void TakeDamage(float dmg)
         {
-            health = Mathf.Max(health - dmg, 0);
-            print(health);
+            healthPoints = Mathf.Max(healthPoints - dmg, 0);
+            if(healthPoints == 0)
+            {
+                Die();
+            }
+            print(healthPoints);
+        }
+
+        private void Die()
+        {
+            if (!isAlive) return;
+            animator.SetTrigger(DEATH_TRIGGER);
+            isAlive = false;
         }
     }
 }
